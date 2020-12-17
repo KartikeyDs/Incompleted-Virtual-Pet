3c52c11726f283ded7bf6abb9d28dfc2cbe0013d
KartikeyDs <70206713+KartikeyDs@users.noreply.github.com>
2020-12-17 22:55:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+//Create variables here
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
